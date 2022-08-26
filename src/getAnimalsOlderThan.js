308aa1661fe8b6f9412dedbff2e29aa595a99623
Ellen Sampaio <ellenssbarross@gmail.com>
2022-08-26 19:01:32 -0300

    Adiciona requisito 2 concluido

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  return data.species.every((element) => )
+  // const nome = species
+  //   .find((me) => me.name === animal)
+  //   .residents.every((element) => element.age >= age);
+
+  // return nome;
+  const nome = species[0].residents;
+  const idade = species[0].residents;
+
+  return (
+    nome.every((element) => element.name === animal) || idade
+      .every((elemento) => elemento.age >= age)
+  );
 }
+
+console.log(getAnimalsOlderThan('lions', 15));
 
 module.exports = getAnimalsOlderThan;
