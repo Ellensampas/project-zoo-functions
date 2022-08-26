dc1091e9cef3fc724243b44dbd6d30f342e23999
Ellen Sampaio <ellenssbarross@gmail.com>
2022-08-26 16:59:26 -0300

    Adiciona requisito 1 concluido

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species.every((element) => )
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,12 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids, id) {
-  return data.species.filter((element) => {
-    const semID = [];
-    return ids === element.id || id === element.id
-      ? element.name
-      : (ids === '' || id === '') === semID;
-  });
+function getSpeciesByIds(...ids) {
+  return data.species.filter((animal) => ids.find((anima) => anima === animal.id));
 }
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+
+// console.log(getSpeciesByIds('78460a91-f4da-4dea-a469-86fd2b8ccc84'));
+
 module.exports = getSpeciesByIds;
