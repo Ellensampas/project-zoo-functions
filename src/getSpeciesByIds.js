caa6b1554cab95336922693f92a4ca5d4b8c06e6
Ellen Sampaio <ellenssbarross@gmail.com>
2022-08-26 16:05:18 -0300

    Adiciona requisito 1 concluido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(ids, id) {
+  return data.species.filter((element) => {
+    const semID = [];
+    return ids === element.id || id === element.id
+      ? element.name
+      : (ids === '' || id === '') === semID;
+  });
 }
-
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
