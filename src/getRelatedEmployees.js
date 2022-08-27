741e747de911937210f4f39becc00dc477932fbf
Ellen Sampaio <ellenssbarross@gmail.com>
2022-08-26 21:08:12 -0300

    Adiciona requisito 4 concluido

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((pessoa) => pessoa.managers.find((pessoaId) => pessoaId === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) !== true) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees.filter((pessoa) => pessoa.managers.includes(managerId))
+    .map((element) => `${element.firstName} ${element.lastName}`);
 }
+console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 module.exports = { isManager, getRelatedEmployees };
