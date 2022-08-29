8a186a40be77caf6b3075fdc7043e8f04a938863
Ellen Sampaio <ellenssbarross@gmail.com>
2022-08-29 19:09:37 -0300

    adiciona requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  let idadeInicial = 0;
+  const array = [];
+  const pessoa = employees.filter((pessoaa) => pessoaa.id === id)
+    .find((animal) => animal).responsibleFor;
+  const animalDaPessoa = species.find((ban) => ban.id === pessoa[0]).residents;
+  animalDaPessoa.forEach((bani) => {
+    if (bani.age > idadeInicial) {
+      idadeInicial = bani.age;
+    }
+  });
+  const alguma = animalDaPessoa.find((age) => age.age === idadeInicial);
+  array.push(alguma.name, alguma.sex, alguma.age);
+  return array;
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
+const { species, hours } = data;
+
+const dias = {
+  Tuesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: [],
+  },
+  Wednesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: [],
+  },
+  Thursday: 'aaaa',
+  Friday: 'aaaa',
+  Saturday: 'aaaa',
+  Sunday: 'aaaa',
+  Monday: 'aaaa',
+};
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (!scheduleTarget) {
+    return dias;
+  }
+  return species.find((animal) => animal.name === scheduleTarget).availability;
 }
+function substituiAnimal(dia) {
+  return hours.find((diass) => diass.hours === getSchedule());
+}
+console.log(substituiAnimal('Tuesday'));
 
 module.exports = getSchedule;
