894fc141916cc5c3e3a094bc4814023e508cff89
Ellen Sampaio <ellenssbarross@gmail.com>
2022-08-29 17:05:34 -0300

    adiciona requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,9 +1,19 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
+const { prices } = data;
+function countEntrants(pessoas) {
+  const adulto = pessoas.filter((adul) => adul.age >= 18 && adul.age < 50).length;
+  const childa = pessoas.filter((kid) => kid.age < 18).length;
+  const senhor = pessoas.filter((senh) => senh.age >= 50).length;
+  return ({ adult: adulto, child: childa, senior: senhor });
 }
 
 function calculateEntry(pessoas) {
+  if (!pessoas || Object.keys(pessoas).length === 0) { return 0; }
+  const peoples = countEntrants(pessoas);
+  return (prices.adult * peoples.adult)
+  + (prices.child * peoples.child)
+  + (prices.senior * peoples.senior);
 }
-console.log(calculateEntry());
+
 module.exports = { calculateEntry, countEntrants };
