b147a50ec2aca600c5bc8f131f4dd9a70593af41
Ellen Sampaio <ellenssbarross@gmail.com>
2022-08-29 23:06:28 -0300

    adiciona requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species } = data;
+
+const capturando = (id, param) => id.map((anima) => (species
+  .find((algo) => algo.id === anima))[param]);
+
+const infos = () => {
+  const relatorio = employees.map((nomes) => {
+    const infosFunci = { id: nomes.id };
+    infosFunci.fullName = `${nomes.firstName} ${nomes.lastName}`;
+    infosFunci.species = capturando(nomes.responsibleFor, 'name');
+    infosFunci.locations = capturando(nomes.responsibleFor, 'location');
+    return infosFunci;
+  });
+  return relatorio;
+};
+function getEmployeesCoverage(name) {
+  if (!name) { return infos(); }
+  const pessoinhas = infos().find((uva) => uva.fullName.includes(name.name) || uva.id === name.id);
+  if (!pessoinhas) throw new Error('Informações inválidas');
+  return pessoinhas;
 }
+// console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
